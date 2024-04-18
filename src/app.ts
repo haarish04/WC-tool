0533102c42161833bc09cf3cfa1ac51f91f49a2c
haarish04 <haarish.anandan@gmail.com>
2024-04-18 14:46:45 +0530

    Added basic starter code to app.ts and kept track of progress in design.txt

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,9 @@ import WC from ".";
 function main() {
   var filePath = process.argv[3];
   var operation = process.argv[2];
-  //console.log(filePath);
-  //console.log(operation);
 
   const res = WC(operation, filePath);
+  console.log(res);
 }
 
 main();
