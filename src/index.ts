2f56ff59e0c29df39009be0be4a407d7d0344293
haarish04 <haarish.anandan@gmail.com>
2024-04-18 14:48:36 +0530

    Update index.ts with bytecount, charactercount, display and wordcount

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,43 @@
 import * as fs from "fs";
+import { Readable } from "stream";
 
-export default function WC(operation: string, filePath: string) {
-  fs.readFileSync("foo.txt", "utf8");
-  return " ";
+function displayFile(file: Readable): void {
+  file.on("data", (chunk) => {
+    console.log(chunk);
+  });
+}
+
+function wordCount(file: Readable): number {
+  var wordcount = 0;
+  file.on("data", (chunk) => {
+    const words = chunk.toString().match(/[a-zA-Z0-9]+/g);
+    wordcount += words.length;
+  });
+  return wordcount;
+}
+
+function lineCount(file: Readable): number {
+  var linecount = 0;
+  return linecount;
+}
+
+function byteCount(file: Readable): number {
+  var bytecount = 0;
+  file.on("data", (chunk) => {
+    bytecount += Buffer.byteLength(chunk);
+  });
+  return bytecount;
+}
+
+function characterCount(file: Readable): number {
+  var charcount = 0;
+  file.on("data", (chunk) => {
+    charcount += chunk.toString().length;
+  });
+  return charcount;
+}
+
+export default function WC(operation: string, filePath: string): string {
+  const file = fs.createReadStream(filePath, "utf8");
+  return "";
 }
